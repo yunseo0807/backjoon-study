c5b3ffdd638ee28e87249569a73b7d0a6f4c32f4
yunseo0807 <rnvl2424@naver.com>
2026-03-24 22:59:09 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,28 @@
+int main(void) {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - 1; j >= i; j--) {
+			printf(" ");
+		}
+		for (int j = 1; j <= 2*i-1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf(" ");
+		}
+		for (int j = n-1; j >=  i; j--) {
+			printf("*");
+		}
+		for (int j = n - 2; j >= i; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
